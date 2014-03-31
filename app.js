c72330d91dbe85dee67f8052450ef5acb53cb4ce
Paul Ballesty <paulballesty@gmail.com>
2014-03-30 23:45:46 -0300

    Moving the project to a path

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var app = require('./setup').app();
 var port = process.env.PORT || 5000;
+
 app.listen(port, function () {
   console.log('Server listening on port ' + port);
+});
+
+app.get("/", function (req, res) {
+  res.send("Nothing here yet!");
 });
